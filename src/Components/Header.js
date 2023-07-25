14f1727c56752ff49bde44d25fb0fb18fe642953
Shreya <118830588+ShreyaA3108@users.noreply.github.com>
2023-07-25 14:04:36 +0545

    creating base file

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+export default function Header() {
+    return (
+      <>
+        <nav className="navbar">
+          <a className="navbar-logo" href="#">
+            mUsIC
+          </a>
+        </nav>
+      </>
+    );
+  }
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,27 @@
+import React, { Component, useState, useEffect } from "react";
+
+import Spotify from "spotify-web-api-js";
+const spotifyWebApi = new Spotify();
+const Main = () => {
+    return (
+        <div className="Main">
+          <div className="mainContainer">
+              <div className="row login">
+                <div id="loginTitle">
+                  <p>
+                    Login With Your <span>Spotify</span> Account
+                  </p>
+                  <p>And Enjoy Meeting People Who </p>
+                  <p>Have The Same Musical Taste As</p>
+                  <p>YOU.</p>
+                </div>
+    
+                  <button>Login With Spotify</button>
+              </div>
+    
+          </div>
+        </div>
+      );
+    };
+    
+    export default Main;
